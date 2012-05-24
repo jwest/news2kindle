a77c5bb8e2254997b44c8a6792406c6c7f549562
Jakub Westfalewski <jwest@jwest.pl>
2012-05-24 22:12:50 +0200

    io - output messages

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 #cooming soon...
 
+Grab your articles from Google Reader, download all articles (no RSS precontent) and send before breakfast to your kindle.
+
 ## Getting started
 ## SetUp CRON
 ## Change storage
--- lib/io.php
+++ lib/io.php
@@ -1,33 +1,88 @@
 <?php
 class IO {
 
-	const COMMAND_LENGTH = 30;
 
+	const COMMAND_LENGTH = 50;
+
+
+	/**
+	 * args for run screen
+	 * @var array
+	 */
 	protected static $args = array();
 
+
+	/**
+	 * Write on screen text line
+	 * @param string $message
+	 * @param bool   $broken_line
+	 */
+	public static function msg($message, $broken_line = TRUE)
+	{
+		echo ( $broken_line ? "\n" : "" ) . $message;
+	}
+
+
+	/**
+	 * Write command for status
+	 * @param string $message
+	 */
 	public static function command($message)
 	{
+		$output_whitespaces = '';
 
+		for ($i = strlen($message); $i <= self::COMMAND_LENGTH; ++$i )
+		{
+			$output_whitespaces .= '-';
+		}
+
+		self::msg( $message . ' ' . $output_whitespaces.' ' );
 	}
 
+
+	/**
+	 * Status - OK
+	 */
 	public static function ok()
 	{
-
+		$colored_string = "\033[1;37m" . "\033[42m" . ' OK ' . "\033[0m";
+		self::msg( $colored_string, FALSE );
 	}
 
-	public static function error($message)
+
+	/**
+	 * Status - Error
+	 */
+	public static function error($message = NULL)
 	{
+		$colored_string = "\033[1;37m" . "\033[41m" . ' ERROR ' . "\033[0m";
+		self::msg( $colored_string, FALSE );
 
+		if ( $message !== NULL )
+		{
+			self::msg( $message );			
+		}
 	}
 
+
+	/**
+	 * Get run args
+	 * @param string $name key config
+	 * @return mixed config value
+	 */
 	public static function arg($name)
 	{
 
 	}
 
+
+	/**
+	 * Prepare args for script
+	 * @param array $args array
+	 */
 	public static function prepare_args($args)
 	{
-		
+
 	}
 
 }
